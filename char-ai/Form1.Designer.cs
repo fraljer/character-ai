e015848fa145d1472949f676b0892b685e46921a
fraljer <samadeka1999@gmail.com>
2025-10-11 19:42:42 +0800

    save data to appdata

--- char-ai/Form1.Designer.cs
+++ char-ai/Form1.Designer.cs
@@ -18,11 +18,18 @@ partial class MainForm
 
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "(character.ai)";
-        this.Icon = char_ai.Resources.cai_logo;    }
+        SuspendLayout();
+        // 
+        // MainForm
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(1366, 786);
+        Icon = Resources.cai_logo;
+        Name = "MainForm";
+        Text = "(character.ai)";
+        ResumeLayout(false);
+    }
 
     #endregion
 }
